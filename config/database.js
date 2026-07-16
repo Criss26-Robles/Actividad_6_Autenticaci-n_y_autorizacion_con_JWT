9faf32eadc49071205b6c200c1dac29d68775e83
argenisgam <argenis.gamboa@tdea.edu.co>
2026-07-16 17:44:11 -0500

    Eliminar node_modules del repositorio y agregar .gitignore

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
 const { Sequelize } = require("sequelize");
+require('dotenv').config();
+console.log(__dirname);
+console.log(process.cwd());
 const sequelize = new Sequelize(
-"biblioteca", // Base de datos
-"postgres", // Usuario
-"123456", // Contraseña
+process.env.DB_DATABASE, // Base de datos
+process.env.DB_USER, // Usuario
+process.env.DB_PASSWORD, // Contraseña
 {
-host: "localhost",
-dialect: "postgres",
+host: process.env.DB_HOST,
+dialect: process.env.DB_DIALECT,
 logging: false
 }
 );
